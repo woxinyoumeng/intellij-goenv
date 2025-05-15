e501d224c250477ca765fbcbecc9c25515dbcfb3
zz <zz4911@163.com>
2025-05-15 10:28:31 +0800

    添加 GOINSECURE 的配置支持

--- src/main/kotlin/com/github/heyvito/goenv/services/EnvParser.kt
+++ src/main/kotlin/com/github/heyvito/goenv/services/EnvParser.kt
@@ -2,13 +2,16 @@ package com.github.heyvito.goenv.services
 
 val commentLineRegexp = Regex("^\\s*#")
 val privateLineRegexp = Regex("\\s*private (.+)")
+val insecureLineRegexp = Regex("\\s*insecure (.+)")
 
 class EnvParser(file: ByteArray) {
     var privates: List<String>
+    var insecures: List<String>
 
     init {
         val fileStr = String(file).split("\n")
         val privates = arrayListOf<String>()
+        val insecures = arrayListOf<String>()
         for (l in fileStr) {
             if (commentLineRegexp matches l) {
                 continue
@@ -19,8 +22,14 @@ class EnvParser(file: ByteArray) {
                 val url = components.elementAt(1)
                 privates.add(url)
             }
+            if (insecureLineRegexp matches l) {
+                val components = insecureLineRegexp.matchEntire(l)!!.groupValues
+                val url = components.elementAt(1)
+                insecures.add(url)
+            }
         }
 
         this.privates = privates
+        this.insecures = insecures
     }
 }
